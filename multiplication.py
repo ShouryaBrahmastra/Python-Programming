e551d5767f5e4e0b3631b8e2f5dda088cc000dd0
Shourya Gupta <gptshourya02@gmail.com>
2021-07-26 15:26:58 +0530

    first upload

---
+++ multiplication.py
@@ -0,0 +1,4 @@
+t=input ("What tables would you like to display? ")
+t=int(t)
+for i in range(10):
+	print (t," X ",i," = ",t*i)
---
+++ sumofno.py
@@ -0,0 +1,4 @@
+answer=0
+for i in range (5):
+	answer=answer+i
+	print ("First ",i,"number, when added, gives ",answer)
